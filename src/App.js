64c86bbb6c9e8e9bd73508f79d172e2ead65eba0
IkramMaududi <onime.kyo999@gmail.com>
2021-07-09 23:48:44 +0700

    obtain data from url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
+/**
+ * 
+ * first name + last name
+ * telephone
+ * email address
+ * country
+ * picture
+ */
 const App = () => {
-  return (
-    <div className="App">
-      <h1>hellosd</h1>
-    </div>
-  );
+    const [dataFromAPI, setdataFromAPI] = useState([]);
+    useEffect( () => {
+        const url = 'https://randomuser.me/api/',
+		      params = { results : 20 };
+
+        const getData = async () => {
+            await Axios
+                    .get(url, { params })
+                    .then(res => {
+						setdataFromAPI(res.data.results);
+						console.log(res.data.results);
+					})
+					.catch(err => console.error('There was an error!', err))
+        };
+        getData();
+    }, []);
+
+    //* process received data
+
+	return (
+		<div className="App">
+			<h1>hellosd</h1>
+		</div>
+	);
 };
 
 export default App;
