aab173131d76ab5dc390b7b0904fa4ed3f4e1f66
IkramMaududi <onime.kyo999@gmail.com>
2021-07-10 09:48:56 +0700

    basic UI; before testing & polishing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import './App.css';
 
-/**
- * 
- * first name + last name
- * telephone
- * email address
- * country
- * picture
- */
 const App = () => {
     const [dataFromAPI, setdataFromAPI] = useState([]);
     useEffect( () => {
@@ -28,12 +20,51 @@ const App = () => {
         getData();
     }, []);
 
-    //* process received data
-
 	return (
-		<div className="App">
-			<h1>hellosd</h1>
-		</div>
+        <>
+            {dataFromAPI.exist ? (
+                <div className="Portfolio">
+                    {dataFromAPI.map((val, index) => {
+                        return (
+                            <div key={index} class="movie-element">
+                                <div class="movie-element__image">
+                                    <img src={val.picture.thumbnail} alt="profile-pict" width="300" height="400"/>
+                                </div>
+                                <div class="movie-element__info">
+                                    <div className="attribute">
+                                        <h2>Name</h2>
+                                        <h2>Email</h2>
+                                        <h2>Telephone-1</h2>
+                                        <h2>Telephone-2</h2>
+                                        <h2>Country</h2>
+                                    </div>
+                                    <div className="semicolon">
+                                        <h2>:</h2>
+                                        <h2>:</h2>
+                                        <h2>:</h2>
+                                        <h2>:</h2>
+                                        <h2>:</h2>
+                                    </div>
+                                    <div className="value">
+                                        <h2>{val.name.title} {val.name.first} {val.name.last}</h2>
+                                        <h2>{val.phone}</h2>
+                                        <h2>{val.cell}</h2>
+                                        <h2>{val.email}</h2>
+                                        <h2>{val.location.country}</h2>
+                                    </div>
+                                    {/* <p>rating</p> */}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            ) : (
+                <div id="top" className="fullSize">
+                    Wait for a while
+                </div>
+            )}
+        </>
+
 	);
 };
 
